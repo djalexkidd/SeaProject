5552c3855456a46aa4b9b27e3dd1b2eddb656d12
Daymortel <daymortel@gmail.com>
2021-10-27 10:46:40 +0200

    change questions

--- report.py
+++ report.py
@@ -14,10 +14,10 @@ p = vlc.MediaPlayer("sounds/music.mp3") # Charge la musique
 p.play() # Lire la musique
 
 # Fonction pour écrire le formulaire dans un fichier CSV
-def write_csv(who, when, where):
+def write_csv(meteo, vent, longitude, latitude):
     with open("report.csv", "a", newline='', encoding='utf-8') as file: # Écrit dans le fichier report.csv
         write = csv.writer(file, quoting = csv.QUOTE_ALL)
-        write.writerow([who.get()] + [] + [where.get()] + [] + [when.get()]) # Écrit le formulaire dans le fichier
+        write.writerow([meteo.get()] + [] + [vent.get()] + [] + [longitude.get()] + [] + [latitude.get()]) # Écrit le formulaire dans le fichier
 
     # Fenêtre du bouton OK
     newWindow = Toplevel(sea)
@@ -38,45 +38,55 @@ def write_csv(who, when, where):
     button_ok = Button(newWindow, text="Ok", command = click_ok, padx = 25, pady = 10)
     button_ok.pack()
 
-# Label à placer à gauche du champ de texte pour Who
-who_info = Label(sea, text="Who are you ?")
-who_info.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
-who_info.place(relx=0.15, rely=0.11, anchor=N)
+# Label à placer à gauche du champ de texte pour la météo
+meteo_info = Label(sea, text="Quel est la météo ? (calme ou agité)")
+meteo_info.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
+meteo_info.place(relx=0.5, rely=0.1, anchor=N)
 
-# Label à placer à gauche du champ de texte pour Where
-where_info = Label(sea, text="Where is it ?")
-where_info.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
-where_info.place(relx=0.15, rely=0.21, anchor=N)
+# Label à placer à gauche du champ de texte pour la vitesse du vent
+vent_info = Label(sea, text="Quel est la vitesse du vent ? (lent ou rapide)")
+vent_info.grid(row = 2, column = 0, columnspan = 3, padx = 10, pady = 10)
+vent_info.place(relx=0.5, rely=0.3, anchor=N)
 
-# Label à placer à gauche du champ de texte pour When
-when_info = Label(sea, text="When is it ?")
-when_info.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
-when_info.place(relx=0.15, rely=0.31, anchor=N)
+# Label à placer à gauche du champ de texte pour la longitude
+longitude_info = Label(sea, text="Quel est votre longitude ?")
+longitude_info.grid(row = 4, column = 0, columnspan = 3, padx = 10, pady = 10)
+longitude_info.place(relx=0.5, rely=0.5, anchor=N)
 
-# Champ de texte à placer pour Who
-who = Entry(sea, width = 50, borderwidth = 5)
-who.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
+# Label à placer à gauche du champ de texte pour la latitude
+latitude_info = Label(sea, text="Quel est votre latitude ?")
+latitude_info.grid(row = 6, column = 0, columnspan = 3, padx = 10, pady = 10)
+latitude_info.place(relx=0.5, rely=0.7, anchor=N)
 
-# Champ de texte à placer pour Where
-where = Entry(sea, width = 50, borderwidth = 5)
-where.grid(row = 1, column = 0, columnspan = 3, padx = 10, pady = 10)
+# Champ de texte à placer pour la météo
+meteo = Entry(sea, width = 50, borderwidth = 5)
+meteo.grid(row = 1, column = 0, columnspan = 3, padx = 10, pady = 10)
 
-# Champ de texte à placer pour When
-when = Entry(sea, width = 50, borderwidth = 5)
-when.grid(row = 2, column = 0, columnspan = 3, padx = 10, pady = 10)
+# Champ de texte à placer pour la vitesse du vent
+vent = Entry(sea, width = 50, borderwidth = 5)
+vent.grid(row = 3, column = 0, columnspan = 3, padx = 10, pady = 10)
 
-button_1 = Button(sea, text="Send", padx = 50, pady = 15, fg = "#00ff00", command = lambda: write_csv(who, when, where)) # Bouton envoyer (lambda envoie les trois valeurs)
+# Champ de texte à placer pour la longitude
+longitude = Entry(sea, width = 50, borderwidth = 5)
+longitude.grid(row = 5, column = 0, columnspan = 3, padx = 10, pady = 10)
+
+# Champ de texte à placer pour la latitude
+latitude = Entry(sea, width = 50, borderwidth = 5)
+latitude.grid(row = 7, column = 0, columnspan = 3, padx = 10, pady = 10)
+
+button_1 = Button(sea, text="Send", padx = 50, pady = 15, fg = "#00ff00", command = lambda: write_csv(meteo, vent, longitude, latitude)) # Bouton envoyer (lambda envoie les quatre valeurs)
 button_2 = Button(sea, text="Exit", padx = 50, pady = 15, fg = "#ff0000", command = sea.destroy) # Bouton quitter
 
-button_1.grid(row = 3, column = 0)
-button_2.grid(row = 3, column = 1)
+button_1.grid(row = 8, column = 0)
+button_2.grid(row = 8, column = 1)
 
 # Mise en page des boutons
-button_1.place(relx=0.4, rely=0.5, anchor=N)
-button_2.place(relx=0.6, rely=0.5, anchor=N)
-who.place(relx=0.5, rely=0.1, anchor=N)
-where.place(relx=0.5, rely=0.2, anchor=N)
-when.place(relx=0.5, rely=0.3, anchor=N)
+button_1.place(relx=0.4, rely=0.9, anchor=N)
+button_2.place(relx=0.6, rely=0.9, anchor=N)
+meteo.place(relx=0.5, rely=0.2, anchor=N)
+vent.place(relx=0.5, rely=0.4, anchor=N)
+longitude.place(relx=0.5, rely=0.6, anchor=N)
+latitude.place(relx=0.5, rely=0.8, anchor=N)
 
 sea.resizable(False, False) # Désactive le redimensionnement de la fenêtre
 
